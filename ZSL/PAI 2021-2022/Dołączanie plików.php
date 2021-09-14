dd41a71dab57a249ca9ffc7fc39076f2c5ae4f29
Jakub <89336968+GrzegorczykJakub@users.noreply.github.com>
2021-09-14 20:34:20 +0200

    Create Dołączanie plików.php
    Lekcja 13/09/21

---
+++ ZSL/PAI 2021-2022/Dołączanie plików.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html> 
+<head>
+
+</head>
+<body>
+    Zawartość strony:<hr>
+    <?php
+    //include '3_1.php'; 
+    require '3_1.php'; //include działa podobnie ale nie kończy po error :)
+    include_once '3_1.php'; ///jeżeli mamy już inne include, to nie doda tej linijki
+    require_once '3_1.php'; ///tak jak powyżej, tylko z require
+    ?>
+    <hr>Koniec pliku
+</body>
+
+</html>
