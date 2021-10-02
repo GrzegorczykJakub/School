e82fa7cac92d45c9bf1a198a9df26b869b660d1d
Jakub <89336968+GrzegorczykJakub@users.noreply.github.com>
2021-10-02 20:07:55 +0200

    Update and rename ZSL/PAI 2021-2022/Formularze2.php to ZSL/PAI 2021-2022/Formularze/Formularze2.php

--- ZSL/PAI 2021-2022/Formularze/Formularze2.php
+++ ZSL/PAI 2021-2022/Formularze/Formularze2.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html lang="pl" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -7,7 +6,6 @@
   </head>
   <body>
     <form action="Formularze2_data.php" method="get">
-
     </form>
   </body>
 </html>
